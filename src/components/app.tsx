2d13cfa5ad17ed2dc1b7ae6e48f2e21734d50d58
limpatrick <contact@patricklim.fr>
2020-08-25 09:48:57 +0200

    add apollo graphql

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,17 +1,43 @@
+import { ApolloClient, ApolloProvider, gql, InMemoryCache } from '@apollo/client';
 import Box from '@material-ui/core/Box';
 import Container from '@material-ui/core/Container';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Typography from '@material-ui/core/Typography';
 import React from 'react';
 
+const client = new ApolloClient({
+  uri: 'https://rickandmortyapi.com/graphql',
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query Episodes {
+        episodes {
+          info {
+            count
+          }
+          results {
+            name
+            episode
+          }
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
+
 const App = () => (
   <>
     <CssBaseline />
-    <Container maxWidth="sm">
-      <Box textAlign="center">
-        <Typography>App</Typography>
-      </Box>
-    </Container>
+    <ApolloProvider client={client}>
+      <Container maxWidth="sm">
+        <Box textAlign="center">
+          <Typography>App</Typography>
+        </Box>
+      </Container>
+    </ApolloProvider>
   </>
 );
 
